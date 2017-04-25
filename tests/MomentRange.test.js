fa3cb78c27fb056dcaff0cae29a89e518a8a8023
Alexey Zabrodsky <elmigranto@gmail.com>
2017-04-25 12:55:41 +0500

    Additional `.mondays()` test.

--- tests/MomentRange.test.js
+++ tests/MomentRange.test.js
@@ -4,6 +4,8 @@ const MomentDate = require('../src/MomentDate');
 const MomentRange = require('../src/MomentRange');
 
 describe('MomentRange', () => {
+  const str = (date) => date._moment.format('D MMM YYYY');
+
   it('is correct and iterable', () => {
     const jan1 = new MomentDate('1 jan');
     const jan2 = new MomentDate('2 jan');
@@ -12,5 +14,12 @@ describe('MomentRange', () => {
     const range = new MomentRange(jan1, jan3, 1, 'day');
     const actual = Array.from(range).map(m => m.millisSinceEpoch());
     expect(actual).to.eql(expected);
+  });
+
+  it('does not go over end date', () => {
+    const start = new MomentDate('10 dec 2017');
+    const end = new MomentDate('31 dec 2017');
+    const range = new MomentRange(start, end, 1, 'month');
+    expect(Array.from(range).map(str)).to.eql(['10 Dec 2017']);
   });
 });
